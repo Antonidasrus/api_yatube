94fd1a10e5d709f3cad764ef311a0f00b4d300b7
Коновалов Антон Романович <akonovalov@stan-group.com>
2023-08-19 16:09:13 +0300

    1st try

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from .views import CommentViewSet, GroupViewSet, PostViewSet
+
+
+router = SimpleRouter()
+
+router.register(r'posts', PostViewSet, basename='posts')
+router.register('posts/(?P<post_id>\\d+)/comments', CommentViewSet,
+                basename='comments')
+router.register(r'groups', GroupViewSet, basename='groups')
+
+
+urlpatterns = [
+    path('v1/', include(router.urls))
+]
